9b5e2e00e9f8c1cbc4b193fa8d8204db7521710c
Léo Biscassi <leo.biscassi@gmail.com>
2018-10-04 12:38:57 -0300

    Adiciona estrutura cassandra e script ETL da base gerp++elem; Adiciona exploração ao dataset gerp++elem no notebook; Refatora scripts da base abraom para se adequarem ao python code style.

---
+++ annovar/src/annovar_gerp_elem.py
@@ -0,0 +1,38 @@
+from pyspark.sql import SparkSession
+
+def get_spark_session(appName = "MI4U"):
+    spark = SparkSession.builder\
+                        .appName(appName)\
+                        .config("spark.jars.packages", "com.datastax.spark:spark-cassandra-connector_2.11:2.3.2")\
+                        .config("spark.cassandra.connection.host", "127.0.0.1")\
+                        .getOrCreate()
+
+    return spark
+
+
+def run_gerp_elem_etl():
+    FILE_PATH = "../data/hg19_gerp++elem.txt"
+    spark = get_spark_session("Annovar gerp++elem ETL")
+
+    hg19_gerp_elem = spark.read.csv(FILE_PATH, header=False, sep="\t")
+    hg19_gerp_elem.createOrReplaceTempView("hg19_gerp_elem")
+
+    hg19_gerp_elem = spark.sql("SELECT SUBSTRING(`_c0`, 4, 10) AS chromosome,"
+              "       `_c1` AS start,"
+              "       `_c2` AS end, "
+              "       `_c3` AS rs_score, "
+              "       `_c4` AS p_value "
+              "FROM hg19_gerp_elem")
+
+    hg19_gerp_elem.write\
+                  .format("org.apache.spark.sql.cassandra")\
+                  .mode("append")\
+                  .options(table="hg19_gerp_elem", keyspace="sequence_databases")\
+                  .save()
+
+    spark.stop()
+
+
+if __name__ == "__main__":
+    run_gerp_elem_etl()
+
